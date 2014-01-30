7a9c73e8978c14e8c8212700951f93ae8605ec13
Alexander Wirt <formorer@debian.org>
2014-01-30 16:49:42 +0100

    Add more variables

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,13 @@
 #
 class puppet 
 (
-  $puppetlabs_repo = $puppet::params::puppetlabs_repo
+ $puppetlabs_repo = $puppet::params::puppetlabs_repo,
+ $master = $puppet::params::master,
+ $run_agent = $puppet::params::run_agent,
+ $report = $puppet::params::report,
+ $pluginsync = $puppet::params::pluginsync,
+ $environment = $puppet::params::environment,
+ $listen = $puppet::params::listen
 )
 {
  # this class does nothing, use puppet::client or puppet::master
