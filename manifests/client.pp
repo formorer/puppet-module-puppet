4cd0932ce2d3066eeb0bde10150601d146b20a56
Alexander Wirt <formorer@debian.org>
2014-01-30 16:49:48 +0100

    Implement some stuff

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,14 +1,52 @@
-class puppet::client (
-  $master = $puppet::params::master,
-  $run_agent = $puppet::params::run_agent,
-  $report = $puppet::params::report,
-  $pluginsync = $puppet::params::pluginsync,
-  $environment = $puppet::params::environment,
-  $listen = $puppet::params::listen
-  )
+class puppet::client inherits puppet
 {
-    Anchor['begin'] ->
-    Class['puppet::install'] ->
-    Anchor['end']
+    anchor {'ntp::client::begin': } ->
+        class {'puppet::install': } ->
+    anchor {'ntp::client::end': }
   
+    $run_agent_param = $run_agent ? {
+        true => 'yes',
+        default => 'no'
+    }
+
+    notify { "${run_agent} ${run_agent_param}": }
+    augeas { "manage_service" :
+        context => "/files/etc/default/puppet",
+        changes => [
+            "set START ${run_agent_param}"
+            ]
+    }
+
+    $run_agent_service = $run_agent ? {
+        true => 'running',
+        default => 'stopped'
+    }
+
+    service { 'puppet':
+        enable => $run_agent,
+        ensure => $run_agent_service
+    }
+
+    Augeas['manage_service'] -> Service['puppet']
+
+
+    if $master {
+        augeas { "puppet_master":
+            context => "/files/etc/puppet/puppet.conf",
+            changes => [
+                "set agent/server ${master}",
+            ]
+        }
+    }
+
+    if $environment {
+     augeas { "puppet_environment":
+            context => "/files/etc/puppet/puppet.conf",
+            changes => [
+                "set agent/environment ${environment}",
+            ]
+        }
+    }
+
+
 }
